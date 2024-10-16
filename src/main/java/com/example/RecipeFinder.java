d20300f5e9ef644c7d37dedd17feccf27c271e61
Karen <karenb.herrera@gmail.com>
2024-10-16 12:53:46 -0400

    Modifying the lambda

--- src/main/java/com/example/RecipeFinder.java
+++ src/main/java/com/example/RecipeFinder.java
@@ -1,9 +1,33 @@
 package com.example;
 
-public class RecipeFinder 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
+public class RecipeFinder implements RequestHandler<Map<String, String>, String>
 {
-    public static void main( String[] args )
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String handleRequest(Map<String, String> event, Context context)
     {
-        System.out.println( "Hello World!" );
+        String ingredient1 = event.get("ingredient1");
+        String ingredient2 = event.get("ingredient2");
+        String ingredientsTotal = ingredient1 + " " + ingredient2;
+
+        //Response
+        Map<String, Object> response = Map.of(
+            "statusCode", 200,
+            "body", "Hello from Lambda," + ingredientsTotal
+        );
+
+        //Convert response to JSON
+        try{
+            return objectMapper.writeValueAsString(response);
+        } catch (Exception e){
+            return "{\"statusCode\":500, \"body\":\"Error\"}";
+        }
     }
 }
